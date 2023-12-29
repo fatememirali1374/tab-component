b3a34a81b222bae712e745ca89ed5287462ed710
fateme Mirali <fatememirali1374@gmail.com>
2023-12-29 19:05:48 +0330

    create small app with react

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import "./App.css";
+const tabData = [
+  {
+    id: 1,
+    title: "ITEM 1",
+    content: "Authorize the user data ✅",
+  },
+  {
+    id: 2,
+    title: "ITEM 2",
+    content: "Redirect user to cart page 🛒",
+  },
+  {
+    id: 3,
+    title: "ITEM 3",
+    content: "Create new payment for the user 💰",
+  },
+];
+
+function App() {
+ const [activeTab, setActiveTab]=useState(1)
+ const [isOpen, setIsOpen]=useState(true)
+
+ const handelActiveTab=(id)=>{
+  setActiveTab(id)
+ }
+return (
+ <div>
+  <button 
+  onClick={()=>setIsOpen((is)=>!is)}>❌</button>
+ {
+  isOpen ? ( <div className="tab">
+  <div className="tab__header">
+{tabData.map((tab)=>{
+  return(
+    <button
+  onClick={()=>handelActiveTab(tab.id)}
+  key={tab.id} className={activeTab===tab.id?"active":""}>
+    <span>{tab.title}</span>
+    <span className="tab-indicator"></span>
+  </button>
+  )
+
+})}
+
+  </div>
+  <div className="tab__content">{tabData[activeTab-1].content}</div>
+</div>) : (<p>closed.</p>)
+ }
+  
+  
+  </div>
+) 
+ 
+}
+
+export default App;
